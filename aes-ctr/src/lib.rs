2f06fec06e27d3c532bdc914b147c5f56173baf2
Yu Ding <dingelish@gmail.com>
2019-08-19 16:33:28 -0700

    Port to sgx 1.1.0

--- aes-ctr/src/lib.rs
+++ aes-ctr/src/lib.rs
@@ -38,44 +38,44 @@
 //! ```
 #![no_std]
 #![deny(missing_docs)]
-#[cfg(not(all(
-    target_feature = "aes",
-    target_feature = "sse2",
-    target_feature = "ssse3",
-    any(target_arch = "x86_64", target_arch = "x86"),
-)))]
-extern crate aes_soft;
-#[cfg(not(all(
-    target_feature = "aes",
-    target_feature = "sse2",
-    target_feature = "ssse3",
-    any(target_arch = "x86_64", target_arch = "x86"),
-)))]
-extern crate ctr;
+//#[cfg(not(all(
+//    target_feature = "aes",
+//    target_feature = "sse2",
+//    target_feature = "ssse3",
+//    any(target_arch = "x86_64", target_arch = "x86"),
+//)))]
+//extern crate aes_soft;
+//#[cfg(not(all(
+//    target_feature = "aes",
+//    target_feature = "sse2",
+//    target_feature = "ssse3",
+//    any(target_arch = "x86_64", target_arch = "x86"),
+//)))]
+//extern crate ctr;
 pub extern crate stream_cipher;
 
-#[cfg(not(all(
-    target_feature = "aes",
-    target_feature = "sse2",
-    target_feature = "ssse3",
-    any(target_arch = "x86_64", target_arch = "x86"),
-)))]
-mod dummy;
+//#[cfg(not(all(
+//    target_feature = "aes",
+//    target_feature = "sse2",
+//    target_feature = "ssse3",
+//    any(target_arch = "x86_64", target_arch = "x86"),
+//)))]
+//mod dummy;
 
-#[cfg(all(
-    target_feature = "aes",
-    target_feature = "sse2",
-    target_feature = "ssse3",
-    any(target_arch = "x86_64", target_arch = "x86"),
-))]
+//#[cfg(all(
+//    target_feature = "aes",
+//    target_feature = "sse2",
+//    target_feature = "ssse3",
+//    any(target_arch = "x86_64", target_arch = "x86"),
+//))]
 extern crate aesni;
 
-#[cfg(all(
-    target_feature = "aes",
-    target_feature = "sse2",
-    target_feature = "ssse3",
-    any(target_arch = "x86_64", target_arch = "x86"),
-))]
+//#[cfg(all(
+//    target_feature = "aes",
+//    target_feature = "sse2",
+//    target_feature = "ssse3",
+//    any(target_arch = "x86_64", target_arch = "x86"),
+//))]
 use aesni as dummy;
 
 pub use dummy::{Aes128Ctr, Aes192Ctr, Aes256Ctr};
